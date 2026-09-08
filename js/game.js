27e89e632302d1a9f432a9b884851f72976f8f29
Arjun Vyas <arjunvyas2006@gmail.com>
2026-09-08 13:04:04 +0530

    refactor game file names

--- js/game.js
+++ js/game.js
@@ -1,19 +1,3 @@
-// const canvas = document.getElementById("game-canvas");
-// const ctx = canvas.getContext("2d");
-
-// function renderBackground() {
-//   ctx.fillStyle = "#87ceeb";
-//   ctx.fillRect(0, 0, 900, 500);
-
-//   ctx.fillStyle = "#6cab46";
-//   ctx.fillRect(0, 380, 900, 80);
-
-//   ctx.fillStyle = "#8b6945";
-//   ctx.fillRect(0, 460, 900, 40);
-// }
-
-// renderBackground();
-
 const canvas = document.querySelector("canvas");
 
 canvas.width = window.innerWidth;
