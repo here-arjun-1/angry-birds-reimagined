1220444ca948339521a4e418184cb5b5280a78c1
Arjun Vyas <arjunvyas2006@gmail.com>
2026-09-12 03:24:09 +0530

    feat: boundary limitations removed

--- js/game.js
+++ js/game.js
@@ -443,14 +443,6 @@ function updateBirdPhysics() {
       setTimeout(resetBird, 500);
     }
   }
-  if (bird.x - bird.radius <= 0) {
-    bird.x = bird.radius;
-    bird.vx *= -0.5;
-  }
-  if (bird.y - bird.radius <= 0) {
-    bird.y = bird.radius;
-    bird.vy *= -0.5;
-  }
   if (bird.y > canvas.height + 200) {
     bird.launch = false;
     setTimeout(resetBird, 300);
